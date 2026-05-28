8edf8e4116905211f4e5df12d02e9de321b5b9c6
Sandeep Behera <thesbcode@gmail.com>
2026-05-29 01:21:39 +0530

    Exploring Text-Clip property in tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './component/Test'
+import './index.css'
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-black bg-opacity-50">
+     <Test />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+      <div className={`text-center grid w-full h-screen place-items-center `}>
+        <h1 className="bg-[url('/Space.png')] text-9xl font-bold bg-cover text-transparent bg-clip-text animate-slowpan">Exploring the Space</h1>
+      </div>
+    </>
+  )
+}
+
+export default Test
